5adf281369a2483e629fdcbc9a4af6cfc3c4ab7c
pkage <quadnix1@gmail.com>
2019-05-06 10:32:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PassCalc
+
+A really, really simple calculator to figure out how many points you need to pass.
+
+Written in haste during exams.
---
+++ index.js
@@ -0,0 +1,46 @@
+const computeTweetUrl = (score) => {
+	const anchor = document.querySelector('#tweetthis')
+
+	const uri = `https://twitter.com/intent/tweet?text=I need a ${score} to pass my exams 😱`
+
+	anchor.setAttribute('href', encodeURI(uri))
+}
+
+const calculateFinalScore = (weight, cw) => {
+	weight = document.querySelector('#weighting').value
+	cw     = document.querySelector('#coursework').value
+
+	weight = weight / 100
+	cw     = cw / 100
+
+	let finalScore = ( 0.40 - ( cw * (1 - weight)) ) / weight
+
+	finalScore = Math.max(finalScore, 0)
+	finalScore = (finalScore * 100).toFixed(1)
+
+	document.querySelector('#final').innerText = finalScore
+	computeTweetUrl(finalScore)
+
+}
+
+const updateWeighting = () => {
+	const exam_weight = document.querySelector('#weighting').value
+
+	// update readouts
+	document.querySelector('#exam_weight').innerText = exam_weight
+	document.querySelector('#coursework_weight').innerText = 100 - exam_weight
+
+	requestAnimationFrame(calculateFinalScore)
+}
+
+const updateCoursework = () => {
+	requestAnimationFrame(calculateFinalScore)
+}
+
+
+// attach events
+document.querySelector('#weighting').addEventListener('change', updateWeighting)
+document.querySelector('#weighting').addEventListener('input', updateWeighting)
+document.querySelector('#coursework').addEventListener('change', updateCoursework)
+document.querySelector('#coursework').addEventListener('keyup', updateCoursework)
+calculateFinalScore()
